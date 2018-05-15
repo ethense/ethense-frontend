692badc21c07e646b7902cccf50a57d049eafa20
Mike Xu <mxu11290@gmail.com>
2018-05-15 14:14:55 -0400

    test login form for submit login and toggle login mode buttons

--- src/components/LoginForm/LoginForm.js
+++ src/components/LoginForm/LoginForm.js
@@ -2,9 +2,18 @@ import React from 'react'
 import { withTheme } from 'material-ui/styles'
 import Paper from 'material-ui/Paper'
 import TextField from 'material-ui/TextField'
+import Button from 'material-ui/Button'
 import { InputAdornment } from 'material-ui/Input'
 import Icon from 'material-ui/Icon'
 import styled from 'styled-components'
+
+const StyledPaper = withTheme()(styled(Paper)`
+  display: flex;
+  flex-direction: column;
+  width: 380px;
+  height: 420px;
+  padding: ${props => props.theme.spacing.medium};
+`)
 
 const StyledTextField = withTheme()(styled(({ endIcon, ...other }) => (
   <TextField
@@ -28,28 +37,63 @@ const StyledTextField = withTheme()(styled(({ endIcon, ...other }) => (
   }
 `)
 
-const StyledPaper = withTheme()(styled(Paper)`
-  display: flex;
-  flex-direction: column;
-  width: 380px;
-  padding: ${props => props.theme.spacing.medium};
+const StyledButton = withTheme()(styled(props => (
+  <Button {...props} />
+))`
+  background-image: linear-gradient(
+    45deg,
+    ${props => !props.disabled && props.theme.palette.primary.main} 10%,
+    ${props => !props.disabled && props.theme.palette.secondary.main} 90%
+  );
+
+  color: ${props => !props.disabled && props.theme.palette.grey['50']};
+
+  &:hover {
+    background-image: linear-gradient(
+      45deg,
+      ${props => !props.disabled && props.theme.palette.primary.dark} 10%,
+      ${props => !props.disabled && props.theme.palette.secondary.dark} 90%
+    );
+  }
+`)
+
+const FlexSpace = withTheme()(styled.div`
+  flex: 1;
+  position: relative;
+
+  &:before {
+    content: '';
+    position: absolute;
+    top: 50%;
+    left: 0;
+    border-top: 1px solid ${props => props.theme.palette.grey['200']};
+    background: block;
+    width: 100%;
+  }
 `)
 
 const LoginForm = () => (
   <StyledPaper data-test-id="loginForm" elevation={1}>
     <StyledTextField
-      fullWidth
       data-test-id="emailInput"
+      fullWidth
       label="Email"
       endIcon="email"
     />
     <StyledTextField
-      fullWidth
       data-test-id="passwordInput"
+      fullWidth
       type="password"
       label="Password"
       endIcon="lock"
     />
+    <StyledButton data-test-id="submitLogin" fullWidth variant="raised">
+      Login
+    </StyledButton>
+    <FlexSpace />
+    <Button data-test-id="toggleLoginMode" fullWidth variant="raised">
+      Login with uPort
+    </Button>
   </StyledPaper>
 )
 
--- src/components/LoginForm/LoginForm.test.js
+++ src/components/LoginForm/LoginForm.test.js
@@ -8,5 +8,7 @@ describe('LoginForm', () => {
     expect(component.exists()).toEqual(true)
     expect(component.find('[data-test-id="emailInput"]').length).toEqual(1)
     expect(component.find('[data-test-id="passwordInput"]').length).toEqual(1)
+    expect(component.find('[data-test-id="submitLogin"]').length).toEqual(1)
+    expect(component.find('[data-test-id="toggleLoginMode"]').length).toEqual(1)
   })
 })
