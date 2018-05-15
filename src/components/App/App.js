5b3d35d4b326aabf0ea56a328d9d85aa0557dea8
Mike Xu <mxu11290@gmail.com>
2018-05-14 23:28:06 -0400

    refactor login form out of app into a component

---
+++ src/components/App/App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import LoginForm from '../LoginForm'
+
+const App = () => (
+  <div>
+    <LoginForm />
+  </div>
+)
+
+export default App
---
+++ src/components/LoginForm/LoginForm.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const LoginForm = () => (<div data-test-id="loginForm" />)
+
+export default LoginForm
