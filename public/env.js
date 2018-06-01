d3920589bb58e42d4f06baecd70e39b91d83d897
Mike Xu <mxu11290@gmail.com>
2018-05-31 22:01:20 -0400

    set up runtime environment variables in public/env.js

---
+++ public/env.js
@@ -0,0 +1,13 @@
+// This file will not end up inside the main application JavaScript bundle.
+// Instead, it will simply be copied inside the build folder.
+// The generated "index.html" will require it just before this main bundle.
+// You can thus use it to define some environment variables that will
+// be made available synchronously in all your JS modules under "src".
+//
+// Warning: this file will not be transpiled by Babel and cannot contain
+// any syntax that is not yet supported by your targeted browsers.
+
+window.env = {
+  REACT_APP_API_URL: 'http://localhost:3000/api',
+  REACT_APP_AUTH_KEY: 'auth_ethense',
+}
