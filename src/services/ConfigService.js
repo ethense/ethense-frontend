8bf814c4d7c9d85134cffe72ef86bc2d052a40ea
Mike Xu <mxu11290@gmail.com>
2018-06-18 09:16:03 -0400

    change env vars back to compile time

--- src/services/ConfigService.js
+++ src/services/ConfigService.js
@@ -1,18 +1,10 @@
 class ConfigService {
   getApiServer = () => {
-    const apiServer =
-      window.env && window.env.REACT_APP_API_URL
-        ? window.env.REACT_APP_API_URL
-        : process.env.REACT_APP_API_URL
-    return apiServer || 'http://localhost:3000/api'
+    return process.env.REACT_APP_API_URL || 'http://localhost:3000/api'
   }
 
   getAuthKey = () => {
-    const authKey =
-      window.env && window.env.REACT_APP_AUTH_KEY
-        ? window.env.REACT_APP_AUTH_KEY
-        : process.env.REACT_APP_AUTH_KEY
-    return authKey || 'auth_ethense'
+    return process.env.REACT_APP_AUTH_KEY || 'auth_ethense'
   }
 }
 
