7ec53b661194c851fa2366138686c9d7ded6df0a
Mike Xu <mxu11290@gmail.com>
2018-07-17 16:01:26 -0400

    add UI for managing app ids

--- src/components/ManageAppIds/ManageAppIds.js
+++ src/components/ManageAppIds/ManageAppIds.js
@@ -1,19 +1,90 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux'
+import PropTypes from 'prop-types'
 import { Typography } from '@material-ui/core'
+import Paper from '@material-ui/core/Paper'
+import Table from '@material-ui/core/Table'
+import TableHead from '@material-ui/core/TableHead'
+import TableRow from '@material-ui/core/TableRow'
+import TableCell from '@material-ui/core/TableCell'
+import TableBody from '@material-ui/core/TableBody'
+import IconButton from '@material-ui/core/IconButton'
+import Icon from '@material-ui/core/Icon'
 
 import { SidebarLayout } from '../../layouts'
 import { GradientButton, PageHeader } from '../elements'
+import { getAppIds, addAppId, deleteAppId } from '../../modules/appIdentity'
+import AddAppIdDialog from '../AddAppIdDialog'
 
+const defaultState = {
+  appIdDialogOpen: false,
+}
 export class ManageAppIds extends Component {
+  state = { ...defaultState }
+
+  componentWillMount() {
+    this.props.getAppIds()
+  }
+
+  handleOpenAppIdDialog = () => {
+    this.setState({ appIdDialogOpen: true })
+  }
+  handleCloseAppIdDialog = () => {
+    this.setState({ appIdDialogOpen: false })
+  }
+  handleAddAppId = values => {
+    this.props.addAppId(values)
+    this.setState({ appIdDialogOpen: false })
+  }
+  handleDeleteAppId = id => () => {
+    this.props.deleteAppId(id)
+  }
+
   render() {
     return (
       <SidebarLayout>
         <PageHeader>
           <Typography variant="title">App Identities</Typography>
-          <GradientButton data-test-id="addAppIdBtn" variant="raised">
+          <GradientButton
+            data-test-id="addAppIdBtn"
+            variant="raised"
+            onClick={this.handleOpenAppIdDialog}
+          >
             Add Identity
           </GradientButton>
         </PageHeader>
+        <AddAppIdDialog
+          open={this.state.appIdDialogOpen}
+          onClose={this.handleCloseAppIdDialog}
+          onSubmit={this.handleAddAppId}
+        />
+        <Paper elevation={1}>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name</TableCell>
+                <TableCell>Address</TableCell>
+                <TableCell>Private Key</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody data-test-id="appIdsTableBody">
+              {this.props.appIds.map(appId => (
+                <TableRow key={appId.id}>
+                  <TableCell component="th" scope="row">
+                    {appId.name}
+                  </TableCell>
+                  <TableCell>{appId.mnid}</TableCell>
+                  <TableCell>{appId.privateKey}</TableCell>
+                  <TableCell numeric>
+                    <IconButton onClick={this.handleDeleteAppId(appId.id)}>
+                      <Icon>delete_outline</Icon>
+                    </IconButton>
+                  </TableCell>
+                </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
       </SidebarLayout>
     )
   }
@@ -21,4 +92,26 @@ export class ManageAppIds extends Component {
 
 ManageAppIds.route = '/appIdentities'
 
-export default ManageAppIds
+ManageAppIds.propTypes = {
+  appIds: PropTypes.array,
+  getAppIds: PropTypes.func.isRequired,
+  addAppId: PropTypes.func.isRequired,
+  // removeAppId: PropTypes.func.isRequired,
+}
+
+export default connect(
+  state => ({
+    appIds: state.appIdentity.identities,
+  }),
+  dispatch => ({
+    getAppIds() {
+      dispatch(getAppIds())
+    },
+    addAppId(values) {
+      dispatch(addAppId(values))
+    },
+    deleteAppId(id) {
+      dispatch(deleteAppId(id))
+    },
+  })
+)(ManageAppIds)
--- src/modules/appIdentity.js
+++ src/modules/appIdentity.js
@@ -9,6 +9,10 @@ export const ADD_APP_ID_REQUEST = 'appIdentity/ADD_APP_ID_REQUEST'
 export const ADD_APP_ID_SUCCESS = 'appIdentity/ADD_APP_ID_SUCCESS'
 export const ADD_APP_ID_FAILURE = 'appIdentity/ADD_APP_ID_FAILURE'
 
+export const DELETE_APP_ID_REQUEST = 'appIdentity/DELETE_APP_ID_REQUEST'
+export const DELETE_APP_ID_SUCCESS = 'appIdentity/DELETE_APP_ID_SUCCESS'
+export const DELETE_APP_ID_FAILURE = 'appIdentity/DELETE_APP_ID_FAILURE'
+
 const getAppIdsRequest = () => ({ type: GET_APP_IDS_REQUEST })
 const getAppIdsSuccess = response => ({
   type: GET_APP_IDS_SUCCESS,
@@ -29,6 +33,16 @@ const addAppIdFailure = error => ({
   payload: error,
 })
 
+const deleteAppIdRequest = () => ({ type: DELETE_APP_ID_REQUEST })
+const deleteAppIdSuccess = response => ({
+  type: DELETE_APP_ID_SUCCESS,
+  payload: response,
+})
+const deleteAppIdFailure = error => ({
+  type: DELETE_APP_ID_FAILURE,
+  payload: error,
+})
+
 // state
 const initialState = {
   identities: [],
@@ -41,10 +55,19 @@ export default (state = initialState, action = {}) => {
   switch (action.type) {
     case GET_APP_IDS_REQUEST:
     case ADD_APP_ID_REQUEST:
+    case DELETE_APP_ID_REQUEST:
       return {
         ...state,
         reading: true,
         error: null,
+      }
+    case GET_APP_IDS_FAILURE:
+    case ADD_APP_ID_FAILURE:
+    case DELETE_APP_ID_FAILURE:
+      return {
+        ...state,
+        reading: false,
+        error: action.payload,
       }
     case GET_APP_IDS_SUCCESS:
       return {
@@ -52,19 +75,20 @@ export default (state = initialState, action = {}) => {
         reading: false,
         identities: action.payload,
       }
-    case GET_APP_IDS_FAILURE:
-    case ADD_APP_ID_FAILURE:
+    case ADD_APP_ID_SUCCESS:
+      const newAppId = action.payload
       return {
         ...state,
         reading: false,
-        error: action.payload,
+        identities: [...state.identities, newAppId],
       }
-    case ADD_APP_ID_SUCCESS:
-    return {
-      ...state,
-      reading: false,
-      identities: action.payload,
-    }
+    case DELETE_APP_ID_SUCCESS:
+      const deletedAppId = action.payload
+      return {
+        ...state,
+        reading: false,
+        identities: state.identities.filter(appId => appId.id !== deletedAppId),
+      }
     default:
       return state
   }
@@ -89,10 +113,22 @@ export const addAppId = values => async dispatch => {
   dispatch(addAppIdRequest())
   try {
     const response = await api.post('/AppIds', values)
-    dispatch(addAppIdSuccess([response.data]))
+    dispatch(addAppIdSuccess(response.data))
     return response
   } catch (error) {
     dispatch(addAppIdFailure(error))
+    return error
+  }
+}
+
+export const deleteAppId = id => async dispatch => {
+  dispatch(deleteAppIdRequest())
+  try {
+    const response = await api.delete(`/AppIds/${id}`)
+    dispatch(deleteAppIdSuccess(id))
+    return response
+  } catch (error) {
+    dispatch(deleteAppIdFailure(error))
     return error
   }
 }
